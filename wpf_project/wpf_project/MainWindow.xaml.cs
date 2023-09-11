60adc2f411cdb210e1da24017f1b5c4138f65c5d
Momo <momoguirassy224@gmail.com>
2023-09-11 16:51:52 +0200

    creation de projet Bienvenue

---
+++ wpf_project/wpf_project/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string personNameValue = NomP.Text;
+            string personLastNameValue = PrenomP.Text;
+
+            string generateSentence = $"Bienvenue Mr {personNameValue} : {personLastNameValue}";
+
+            resultat.Content = generateSentence ;
+        }
+    }
+}
